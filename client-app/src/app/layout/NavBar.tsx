dd08d92f75cafc9d8f14acbd5741fbfc286b64b7
rajbanshirahul <rahul.rajbanshi17@gmail.com>
2022-12-22 21:43:26 +0545

    Add: client-app: routing

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,22 +1,20 @@
 import React from "react";
 import { observer } from 'mobx-react-lite';
 import { Button, Container, Menu } from "semantic-ui-react";
-import { useStore } from '../stores/store';
-
+import { NavLink } from 'react-router-dom';
 
 export default observer(function NavBar() {
-    const { activityStore } = useStore();
 
     return (
         <Menu inverted fixed='top'>
             <Container>
-                <Menu.Item header>
+                <Menu.Item as={NavLink} to='/' exact header>
                     <img src="/assets/logo.png" alt="logo" style={{ marginRight: '10px' }} />
                     Reactivities
                 </Menu.Item>
-                <Menu.Item name='Activities' />
+                <Menu.Item as={NavLink} to='/activities' name='Activities' />
                 <Menu.Item>
-                    <Button onClick={() => activityStore.openForm()} positive content='Create Activity' />
+                    <Button as={NavLink} to='/createActivity' positive content='Create Activity' />
                 </Menu.Item>
             </Container>
         </Menu>
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,13 +1,18 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { Button, Form, Segment } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
+import { Link, useHistory, useParams } from 'react-router-dom';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { v4 as uuid } from 'uuid';
 
 export default observer(function ActivityForm() {
+    const history = useHistory();
     const { activityStore } = useStore();
-    const { selectedActivity, closeForm, createActivity, updateActivity, loading } = activityStore;
+    const { createActivity, loading, loadActivity, updateActivity, loadingInitial } = activityStore;
+    const { id } = useParams<{ id: string }>();
 
-    const initialState = selectedActivity ?? {
+    const [activity, setActivity] = useState({
         id: '',
         title: '',
         category: '',
@@ -15,18 +20,31 @@ export default observer(function ActivityForm() {
         date: '',
         city: '',
         venue: ''
-    }
+    });
 
-    const [activity, setActivity] = useState(initialState);
+    useEffect(() => {
+        if (id) loadActivity(id).then(activity => setActivity(activity!));
+    }, [id, loadActivity]);
 
     function handleSubmit() {
-        activity.id ? updateActivity(activity) : createActivity(activity);
+        if (activity.id.length === 0) {
+            let newActivity = {
+                ...activity,
+                id: uuid()
+            };
+            createActivity(newActivity).then(() => history.push(`/activities/${newActivity.id}`));
+        } else {
+            updateActivity(activity).then(() => history.push(`/activities/${activity.id}`));
+        };
+
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
         const { name, value } = event.target;
         setActivity({ ...activity, [name]: value });
     }
+
+    if (loadingInitial) return <LoadingComponent content='Loading activity...' />
 
     return (
         <Segment clearing>
@@ -38,8 +56,8 @@ export default observer(function ActivityForm() {
                 <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange} />
                 <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange} />
                 <Button loading={loading} floated='right' positive type='submit' content='Submit' />
-                <Button onClick={closeForm} floated='right' positive type='button' content='Cancel' />
+                <Button as={Link} to='/activities' floated='right' positive type='button' content='Cancel' />
             </Form>
         </Segment>
     );
-})
+});
